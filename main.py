539e2a5a7396d59a7b28660aae6b53d36c9a274b
Стас Робул <stas.robul@gmail.com>
2022-11-07 16:28:25 +0700

    Add docstring

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from random import randint
 
-from graphic_arts.graphic_arts.start_game_banner import run_screensaver
-
 
 def attack(char_name: str, char_class: str) -> str:
+    """Вызывает функцию атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
                 f' {5 + randint(3, 5)}')
@@ -16,6 +15,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Вызывает функцию защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +25,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Открывает специальные возможности игрока."""
     if char_class == 'warrior':
         return (f'{char_name} применил умение «Выносливость {80 + 25}»')
     if char_class == 'mage':
@@ -34,6 +35,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Открывает режим тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -58,6 +60,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Режим выбора персонажа."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
